fed380ff58c14856fd4d2b5b9ea3de9237ebadaa
Smriti <smritiangel95@gmail.com>
2024-06-24 08:13:17 +0530

    Jenkins-project

---
+++ Jenkins-project/src/test/java/com/smriti/Jenkins_project/JenkinsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smriti.Jenkins_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
